a55dc19b541e9cebd0e7dfaa6aa558a05abf0dee
Greg Kapka <greg@oraclize.it>
2020-08-07 11:46:53 +0100

    ref(pk-getter): <- rename that for clarity

--- src/lib/decrypt_ethereum_private_key.rs
+++ src/lib/decrypt_ethereum_private_key.rs
@@ -21,7 +21,7 @@ fn check_keyfile_exists(keyfile_path: &str) -> Result<()> {
     }
 }
 
-fn maybe_get_eth_private_key(keyfile_path: &str) -> Result<String> {
+fn maybe_decrypt_ethereum_private_key(keyfile_path: &str) -> Result<String> {
     info!("✔ Decrypting private key...");
     let output = Command::new("gpg").arg("-d").arg(keyfile_path).output()?;
     match output.stdout.len() {
@@ -39,15 +39,15 @@ fn maybe_get_eth_private_key(keyfile_path: &str) -> Result<String> {
 fn get_eth_private_key_from_hex(hex: &str) -> Result<EthereumKeys> {
     info!("✔ Creating Eth private key from hex...");
     let eth_pk = EthereumKeys::from_hex_private_key(&hex)?;
-    info!("✔ ETH address: '{}'", eth_pk.to_address());
+    info!("✔ ETH address: 0x{}", &eth_pk.address_string);
     Ok(eth_pk)
 }
 
-pub fn get_eth_private_key_and_add_to_state(state: State) -> Result<State> {
+pub fn decrypt_ethereum_private_key_and_add_to_state(state: State) -> Result<State> {
     info!("✔ Maybe getting ETH private key & adding to state...");
     let keyfile_path = &state.cli_args.flag_keyfile;
     check_keyfile_exists(keyfile_path)
-        .and_then(|_| maybe_get_eth_private_key(keyfile_path))
+        .and_then(|_| maybe_decrypt_ethereum_private_key(keyfile_path))
         .and_then(|hex| get_eth_private_key_from_hex(&hex))
-        .and_then(|eth_pk| state.add_eth_private_key(eth_pk))
+        .and_then(|eth_pk| state.add_ethereum_private_key(eth_pk))
 }
