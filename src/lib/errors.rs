e605eee53540a994d22d72dab77f95b5775febad
Greg Kapka <greg@oraclize.it>
2020-08-11 09:56:13 +0100

    feat(errors): adds `None` err to `AppError` enum

--- src/lib/errors.rs
+++ src/lib/errors.rs
@@ -6,6 +6,7 @@ pub enum AppError {
     IOError(std::io::Error),
     CryptoError(secp256k1::Error),
     UTF8Error(std::str::Utf8Error),
+    NoneError(std::option::NoneError),
 }
 
 impl fmt::Display for AppError {
@@ -14,6 +15,7 @@ impl fmt::Display for AppError {
             AppError::Custom(ref msg) => msg.to_string(),
             AppError::IOError(ref e) => format!("✘ I/O error: {}", e),
             AppError::UTF8Error(ref e) => format!("✘ UTF8 error: {}", e),
+            AppError::NoneError(ref e) => format!("✘ None error: {:?}", e),
             AppError::CryptoError(ref e) => format!("✘ Crypto error: {}", e),
 
         };
@@ -36,5 +38,11 @@ impl From<std::str::Utf8Error> for AppError {
 impl From<std::io::Error> for AppError {
     fn from(e: std::io::Error) -> AppError {
         AppError::IOError(e)
+    }
+}
+
+impl From<std::option::NoneError> for AppError {
+    fn from(e: std::option::NoneError) -> AppError {
+        AppError::NoneError(e)
     }
 }
