ea2f8932150da7e4ad7c54c2a6432c3276ec3130
Greg Kapka <greg@oraclize.it>
2020-08-12 15:46:11 +0100

    feat(constants): <- adds more eth-related of those

--- src/lib/constants.rs
+++ src/lib/constants.rs
@@ -1,2 +1,5 @@
+pub const ETH_NUM_DECIMALS: usize = 18;
+pub const ONE_GWEI: u64 = 1_000_000_000;
 pub const PRIVATE_KEY_HEX_LENGTH: usize = 64;
 pub const TOOL_VERSION: &str = env!("CARGO_PKG_VERSION");
+pub const ONE_ETH_IN_WEI: u64 = 1_000_000_000_000_000_000;
