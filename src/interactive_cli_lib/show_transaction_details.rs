b35fea4f5bfe85b8085214aa9bdccb222217a8f7
Greg Kapka <greg@oraclize.it>
2020-08-12 16:36:42 +0100

    fix(tx-details): rm panicking use of u64 in final tx calculations

--- src/interactive_cli_lib/show_transaction_details.rs
+++ src/interactive_cli_lib/show_transaction_details.rs
@@ -11,13 +11,13 @@ pub fn show_transaction_details(state: InteractiveCliState) -> Result<Interactiv
     let gas_limit_wei = &state.eth_gas_limit?;
     let gas_price_in_wei = &state.eth_gas_price_wei?;
     let total_cost_in_wei = amount_in_wei.saturating_add(gas_limit_wei.saturating_mul(*gas_price_in_wei));
-    let total_cost_in_eth = total_cost_in_wei.as_u64() as f64 / ONE_ETH_IN_WEI as f64;
+    let total_cost_in_eth = total_cost_in_wei.checked_div(*ONE_ETH_IN_WEI)?;
     println!("\n❍ Transaction details:");
     println!("To:         {}", &state.eth_address?);
     println!("Amount:     {} Ξ ({} Wei)", &state.eth_amount.clone()?, amount_in_wei);
     println!("Nonce:      {}", &state.eth_nonce?);
     println!("Gas limit:  {}", gas_limit_wei);
-    println!("Gas price:  {} GWei ({} in Wei)", &state.eth_gas_price_gwei?, gas_price_in_wei);
+    println!("Gas price:  {} GWei ({} Wei)", &state.eth_gas_price_gwei?, gas_price_in_wei);
     println!("Network:    {}", &state.eth_network?);
     println!("Calldata:   {}", format!("0x{}", hex::encode(&state.eth_calldata.clone()?)));
     println!("Total cost: {} Ξ ({} Wei)", total_cost_in_eth, total_cost_in_wei);
